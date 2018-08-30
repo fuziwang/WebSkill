8efd79ec1cfed76db94675de958a96bcb190e4d0
fuziwang <2622860598@qq.com>
2018-08-30 09:35:36 +0800

    02 JavaScript基础语法--语句/注释/区分大小写

---
+++ JavaScript/code/demo01/test02.js
@@ -0,0 +1,19 @@
+// JavaScript语句
+var eventListener = undefined;
+if (2 > 1) {
+    console.log("2 is bigger than 1");
+} else {
+    console.log("2 is smaller than 1");
+}
+
+// JavaScript注释
+// 这是一个单行注释
+/*
+this is a block explaining
+*/
+
+// JavaScript大小写
+var a = 2;
+var A = 2;
+console.log(window.a);
+console.log(window.A);
